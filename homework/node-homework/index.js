ef76b7779462c8fb1067c95399e402efdb62437a
Jervis Bay <jervis.bay@gmail.com>
2020-04-05 09:29:34 -0400

    initial homework files commit

---
+++ homework/node-homework/index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
+const path = require("path");
+const open = require("open");
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {}
+
+function init() {
+
+}
+
+init();
