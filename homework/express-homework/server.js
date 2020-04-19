838911fe4f0020e425dd6fb610b1480378277e9b
Jervis Bay <jervis.bay@gmail.com>
2020-04-19 14:17:12 -0400

    minor edit

--- homework/express-homework/server.js
+++ homework/express-homework/server.js
@@ -17,7 +17,6 @@ fs.readFile("./Develop/db/id.txt", "UTF-8", function(err, res) {
 // Sets up the Express App
 var app = express();
 var portNumber = process.env.PORT || 3000;
-// var portNumber = server.listen(process.env.PORT || 3000);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
